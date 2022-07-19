8f4b515b2f5ee1c09d423ff0f16214006ff2bac9
Simay Güzel <guzelsimay9@gmail.com>
2022-07-19 10:22:31 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from "./components/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState("all");
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    useEffect(() => {
+        getLocalTodos();
+    },[]);
+
+    useEffect(() => {
+        filterHandler();
+        saveLocalTodos();
+    },[todos, status]);
+
+    const filterHandler = () => {
+        switch (status) {
+            case "completed":
+                setFilteredTodos(todos.filter((todo) => todo.completed===true));
+                break;
+            case "uncompleted":
+                setFilteredTodos(todos.filter((todo) => todo.completed===false));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    };
+
+    const saveLocalTodos = () => {
+        if(todos.length !== 0){        //this line is new
+            localStorage.setItem("todos", JSON.stringify(todos))
+        }
+    };
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"))
+            setTodos(todoLocal);
+        }
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <Form
+                inputText={inputText}
+                todos={todos}
+                setTodos={setTodos}
+                setInputText={setInputText}
+                setStatus={setStatus}
+            />
+
+            <TodoList
+                filteredTodos={filteredTodos}
+                setTodos={setTodos}
+                todos={todos}
+            />
+        </div>
+    );
 }
 
 export default App;
